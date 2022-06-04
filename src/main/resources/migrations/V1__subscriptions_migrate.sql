39fd07f6871de26086fff3c2f6db71dd195b6e85
Victor Misael <misael1805@gmail.com>
2022-06-04 10:41:44 -0300

    cria as tabelas usadas nesse serviço

---
+++ src/main/resources/migrations/V1__subscriptions_migrate.sql
@@ -0,0 +1,19 @@
+create schema subscription
+
+    create table if not exists subscription.users
+    (
+        user_id    SERIAl primary key,
+        full_name  varchar(255) not null,
+        dt_created date         not null
+    )
+
+    create table if not exists subscription.subscriptions
+    (
+        subscription_id     SERIAL primary key,
+        user_id             SERIAL not null,
+        subscription_status varchar(100),
+        created_at          date   not null,
+        updated_at          date   not null,
+        constraint fk_user_id FOREIGN KEY (user_id)
+            references subscription.users (user_id)
+    )
