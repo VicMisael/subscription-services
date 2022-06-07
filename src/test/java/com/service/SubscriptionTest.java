2f09476f60218f2d10e21499b6088ab701b9c91b
Victor Misael <misael1805@gmail.com>
2022-06-06 21:17:39 -0300

    Adiciona classe de testes em Subscription

---
+++ src/test/java/com/service/SubscriptionTest.java
@@ -0,0 +1,25 @@
+package com.service;
+
+import com.misael.ascan.microserviceschallenge.DemoApplication;
+import com.misael.ascan.microserviceschallenge.repository.SubscriptionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+
+@SpringBootTest(
+        classes = DemoApplication.class)
+public class SubscriptionTest {
+
+    @Autowired
+    SubscriptionRepository subscriptionRepository;
+
+    @Test
+    public void getEmailByIdTest() {
+        StepVerifier.create(subscriptionRepository.getById(1L)).expectComplete();
+
+    }
+
+
+}
