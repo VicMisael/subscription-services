75ca278cb6ea461e65235f4a115750e1aa998378
Victor Misael <misael1805@gmail.com>
2022-06-06 21:17:19 -0300

    ajusta SubscriptionController.java

--- src/main/java/com/misael/ascan/microserviceschallenge/controller/SubscriptionController.java
+++ src/main/java/com/misael/ascan/microserviceschallenge/controller/SubscriptionController.java
@@ -1,31 +1,45 @@
 package com.misael.ascan.microserviceschallenge.controller;
 
+import com.misael.ascan.microserviceschallenge.model.DTO.SubscriptionDTO;
 import com.misael.ascan.microserviceschallenge.model.Subscription;
 import com.misael.ascan.microserviceschallenge.service.SubscriptionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.misael.ascan.microserviceschallenge.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/subscription")
+@RequiredArgsConstructor
 public class SubscriptionController {
 
-    @Autowired
-    SubscriptionService subscriptionService;
+    private final SubscriptionService subscriptionService;
+    private final UserService userService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Flux<Subscription> allSubscriptions() {
         return subscriptionService.findAll();
     }
 
     @PostMapping()
-    public Mono<Subscription> addSubscription(Subscription subscription) {
-        return subscriptionService.save(subscription);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Subscription> addSubscription(@RequestBody SubscriptionDTO subscription) {
+        return subscriptionService.save(subscription.toSubscription()).flatMap(subs ->
+                userService.getById(subs.getId()).map(s -> subs.withUser(s))).switchIfEmpty(Mono.empty());
     }
 
     @PutMapping()
-    public Mono<Subscription> updateSubscription(Subscription subscription) {
-        return subscriptionService.update(subscription);
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Mono<Subscription> updateSubscription(@RequestBody SubscriptionDTO subscription) {
+        return subscriptionService.update(subscription.toSubscription());
+    }
+
+    @GetMapping("/single")
+    @ResponseStatus(HttpStatus.FOUND)
+    public Mono<Subscription> getById(@RequestParam Long id) {
+        return subscriptionService.find(id);
     }
 }
