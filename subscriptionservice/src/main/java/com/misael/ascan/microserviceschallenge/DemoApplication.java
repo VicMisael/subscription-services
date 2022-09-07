0e400ded41aed37a593474df35187ebb9bee3c0f
Victor Misael <misael1805@gmail.com>
2022-09-07 19:38:45 -0300

    Rollback name to DemoApplication

--- subscriptionservice/src/main/java/com/misael/ascan/microserviceschallenge/DemoApplication.java
+++ subscriptionservice/src/main/java/com/misael/ascan/microserviceschallenge/DemoApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class subscriptionService {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(subscriptionService.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
