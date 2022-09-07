72a261d188cfa6c123961589625c531e4965f970
Victor Misael <misael1805@gmail.com>
2022-09-07 19:35:52 -0300

    muda nome da main da aplicação

--- subscriptionservice/src/main/java/com/misael/ascan/microserviceschallenge/subscriptionService.java
+++ subscriptionservice/src/main/java/com/misael/ascan/microserviceschallenge/subscriptionService.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class subscriptionService {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(subscriptionService.class, args);
     }
 
 }
